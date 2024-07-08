7d6ef36a640d05250594260006cd713be254d159
ginozza <jsimancas@unimagdalena.edu.co>
2024-07-07 22:54:33 -0500

    Corrige error del cambio de tema

--- scripts/app.js
+++ scripts/app.js
@@ -10,11 +10,11 @@ function applyTheme(theme) {
     if (theme === 'dark') {
         themeIcon.classList.replace('fa-moon', 'fa-sun');
         techImage.src = "https://skillicons.dev/icons?i=html,css,javascript,typescript,java,cpp,nodejs,python&theme=dark";
-        toolsImage.src = "https://skillicons.dev/icons?i=git,github,linkedin,vscode,linux,bash,bootstrap,figma&theme=dark";
+        toolsImage.src = "https://skillicons.dev/icons?i=git,github,linkedin,vscode,linux,bash,bootstrap,mysql,figma&theme=dark";
     } else {
         themeIcon.classList.replace('fa-sun', 'fa-moon');
         techImage.src = "https://skillicons.dev/icons?i=html,css,javascript,typescript,java,cpp,nodejs,python&theme=light";
-        toolsImage.src = "https://skillicons.dev/icons?i=git,github,linkedin,vscode,linux,bash,bootstrap,figma&theme=light";
+        toolsImage.src = "https://skillicons.dev/icons?i=git,github,linkedin,vscode,linux,bash,bootstrap,mysql,figma&theme=light";
     }
 }
 
@@ -23,6 +23,7 @@ themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
     const theme = body.classList.contains('dark-theme') ? 'dark' : 'light';
     localStorage.setItem('theme', theme);
+    applyTheme(theme); // Aplicar el tema después de cambiarlo
 });
 
 // Evento: Carga el tema guardado
