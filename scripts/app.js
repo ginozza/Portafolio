2f3ad5d72b2f2844c9c8fce807384e89c34ceea1
ginozza <jsimancas@unimagdalena.edu.co>
2024-07-07 21:38:11 -0500

    Actualiza funcion cambio de tema: Recuerda el tema

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,10 @@ const techImage = document.getElementById('tech-image');
 const toolsImage = document.getElementById('tools-image');
 const body = document.body;
 
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-theme');
-    if (body.classList.contains('dark-theme')) {
+// Funcion: Aplicar tema
+function applyTheme(theme) {
+    body.classList.toggle('dark-theme', theme === 'dark');
+    if (theme === 'dark') {
         themeIcon.classList.replace('fa-moon', 'fa-sun');
         techImage.src = "https://skillicons.dev/icons?i=html,css,javascript,typescript,java,cpp,nodejs,python&theme=dark";
         toolsImage.src = "https://skillicons.dev/icons?i=git,github,linkedin,vscode,linux,bash,bootstrap,figma&theme=dark";
@@ -15,8 +16,24 @@ themeToggle.addEventListener('click', () => {
         techImage.src = "https://skillicons.dev/icons?i=html,css,javascript,typescript,java,cpp,nodejs,python&theme=light";
         toolsImage.src = "https://skillicons.dev/icons?i=git,github,linkedin,vscode,linux,bash,bootstrap,figma&theme=light";
     }
+}
+
+// Evento: Boton cambiar tema
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    const theme = body.classList.contains('dark-theme') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
 });
 
+// Evento: Carga el tema guardado
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        applyTheme(savedTheme);
+    }
+});
+
+// Evento: Link al correo
 document.getElementById('gmail-link').addEventListener('click', function(event) {
     event.preventDefault();
     window.open('https://mail.google.com/mail/?view=cm&fs=1&to=simancasmartinezjuanmanuel@gmail.com', '_blank');
